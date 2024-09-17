df71a289163ca97b3427c3b9de450390c766b87e
SKnabiev <SKnabiev@KB37-309-MMP>
2024-09-17 09:40:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,141 @@
+﻿#include<iostream>
+using namespace std;
+
+
+int main()
+{ 
+	int int1 = 15;
+	int int2 = 20;
+	int int3 = 15;
+	int int4 = 20;
+
+
+	int i = 0;
+	while (i < 10)
+	{
+		i++;
+		cout << "operator true " << endl << "i = " << i << endl ;
+
+
+	}
+
+
+	/*
+	cout <<  "int1 = " << int1 << endl<<"int3 = " << int3<<endl ;
+	if (int2>int1 && int3<int4 ) 
+	{
+		int1++;
+		int3++;
+		cout << "operator true " <<endl<< "int1 = " << int1 <<endl  << "int3 = " << int3 << endl;
+	}
+	else
+	{
+		cout << "operator false " <<endl<< "int1 = " << int1 <<  endl << "int3 = " << int3 << endl;
+	}
+	if (int2 > int1 && int3 < int4)
+	{
+		int1++;
+		int3++;
+		cout << "operator true " <<endl<< "int1 = " << int1 << endl << "int3 = " << int3 << endl;
+	}
+	else
+	{
+		cout << "operator false " <<endl<< "int1 = " << int1 << endl << "int3 = " << int3 << endl;
+	}
+	if (int2 > int1 && int3 < int4)
+	{
+		int1++;
+		int3++;
+		cout << "operator true " << endl << "int1 = " << int1 << endl << "int3 = " << int3 << endl;
+	}
+	else
+	{
+		cout << "operator false " << endl << "int1 = " << int1 << endl << "int3 = " << int3 << endl;
+	}
+	if (int2 > int1 && int3 < int4)
+	{
+		int1++;
+		int3++;
+		cout << "operator true " << endl << "int1 = " << int1 << endl << "int3 = " << int3 << endl;
+	}
+	else
+	{
+		cout << "operator false " << endl << "int1 = " << int1 << endl << "int3 = " << int3 << endl;
+	}
+	if (int2 > int1 && int3 < int4)
+	{
+		int1++;
+		int3++;
+		cout << "operator true " << endl << "int1 = " << int1 << endl << "int3 = " << int3 << endl;
+	}
+	else
+	{
+		cout << "operator false " << endl << "int1 = " << int1 << endl << "int3 = " << int3 << endl;
+	}
+
+	if (int2 > int1 && int3 < int4)
+	{
+		int1++;
+		int3++;
+		cout << "operator true " << endl << "int1 = " << int1 << endl << "int3 = " << int3 << endl;
+	}
+	else
+	{
+		cout << "operator false " << endl << "int1 = " << int1 << endl << "int3 = " << int3 << endl;
+	}
+	if (int2 > int1 && int3 < int4)
+	{
+		int1++;
+		int3++;
+		cout << "operator true " << endl << "int1 = " << int1 << endl << "int3 = " << int3 << endl;
+	}
+	else
+	{
+		cout << "operator false " << endl << "int1 = " << int1 << endl << "int3 = " << int3 << endl;
+	}
+	/*
+	if (int1 == int3)
+	{
+		cout << "operator == " << "true" << endl;
+	}
+	else 
+	{
+		cout << "operator == " << "false" << endl;
+	}
+	if (int1 != int2 )
+	{
+		cout << "operator != " << "true" << endl;
+	}
+	else
+	{
+		cout << "operator == " << "false" << endl;
+	}
+
+	if (int1 == int3 && int2 == int4)
+	{
+
+		cout << "operator == &&  ==  " << "true" << endl;
+	}
+	else
+	{
+		cout << "operator ==  &&  ==  " << "false" << endl;
+	}
+
+
+
+	/*
+	cout << "int1 == int2: " << (int1 == int2) << endl;
+	cout << "int1 != int2: " << (int1 != int2) << endl;
+	cout << "int1 > int2: " << (int1 > int2) << endl;
+	cout << "int1 < int2: " << (int1 < int2) << endl;
+	cout << "int1 >= int2: " << (int1 >= int2) << endl;
+	cout << "int1 <= int2: " << (int1 <= int2) << endl;
+
+		cout << "int1 == int2: " << (int1 == int2) << endl;
+	cout << "int1 != int2: " << (int1 != int2) << endl;
+	cout << "int1 > int2: " << (int1 > int2) << endl;
+	cout << "int1 < int2: " << (int1 < int2) << endl;
+	cout << "int1 >= int2: " << (int1 >= int2) << endl;
+	cout << "int1 <= int2: " << (int1 <= int2) << endl;
+	*/
+	}
